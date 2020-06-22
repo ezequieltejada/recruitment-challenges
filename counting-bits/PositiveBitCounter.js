9cd5c71e40d8c915688afb386be9f67c8d55d17d
Ezequiel Tejada <et.github@outlook.com>
2020-06-22 13:15:17 +0200

    attempt on PositiveBitCounter

--- counting-bits/PositiveBitCounter.js
+++ counting-bits/PositiveBitCounter.js
@@ -1,5 +1,35 @@
-function Count (input) {
-  throw new Error('Not implemented')
+/**
+ * @function Count
+ *
+ * @param {number} input - Number to count its bits on its bit representation.
+ * @returns {array} Containing the amount of bits found on the first index, followed by the index of each bit.
+ */
+function Count(input) {
+  if (input < 0) {
+    throw new RangeError("Cannot be a negative number");
+  }
+  const bitRepresentation = input
+    .toString(2)
+    .split("")
+    .reduce((reversed, character) => character + reversed, "");
+
+  return getIndexes(bitRepresentation);
 }
 
-module.exports = { Count }
+/**
+ * @function getIndexes
+ *
+ * @param {string} bitRepresentation
+ * @returns {array} Containing the amount of bits found on the first index, followed by the index of each bit.
+ */
+function getIndexes(bitRepresentation) {
+  let indexes = [];
+  for (let i = 0; i < bitRepresentation.length; i++) {
+    if (bitRepresentation[i] === "1") {
+      indexes = [...indexes, i];
+    }
+  }
+  return [indexes.length, ...indexes];
+}
+
+module.exports = { Count };
